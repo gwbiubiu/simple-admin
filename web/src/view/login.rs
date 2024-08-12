618858ec70d21b5f616423dfbe24f6529b8226b9
葛威 <gwbiubiu@gmail.com>
2024-08-12 19:20:04 +0800

    fix: use bootstrap ui

--- web/src/view/login.rs
+++ web/src/view/login.rs
@@ -22,7 +22,26 @@ impl Component for Login {
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <div>
-                <ElInput value="abc"></ElInput>
+                // <ElInput value="abc"></ElInput>
+                <table class="table">
+                    <caption>{"基本的表格布局"}</caption>
+                   <thead>
+                      <tr>
+                         <th>{"名称"}</th>
+                         <th>{"城市"}</th>
+                      </tr>
+                   </thead>
+                   <tbody>
+                      <tr>
+                         <td>{"Tanmay"}</td>
+                         <td>{"Bangalore"}</td>
+                      </tr>
+                      <tr>
+                         <td>{"Sachin"}</td>
+                         <td>{"Mumbai"}</td>
+                      </tr>
+                   </tbody>
+                </table>
             </div>
         }
     }

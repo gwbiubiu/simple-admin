e86c28c0dd588128623e7128136bc3c79b56321d
wei.ge <gwbiubiu@gmail.com>
2024-09-10 19:11:03 +0800

    fix: add content icon

--- web/src/containers/header.rs
+++ web/src/containers/header.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use yew_icons::{Icon, IconId};
 
 #[function_component(Header)]
 pub fn header() -> Html {
@@ -6,7 +7,7 @@ pub fn header() -> Html {
         <div class="navbar sticky top-0 bg-base-100 z-10 shadow-md">
             <div class="flex-1">
                 <label for="left-sidebar-drawer" class="btn btn-primary drawer-button lg:hidden">
-                    <i class="fas fa-bars"></i>
+                    <Icon icon_id={IconId::BootstrapJustify} class="h-5 inline-block w-5"/>
                 </label>
                 <h1 class="text-2xl font-semibold ml-2">{"Header"}</h1>
             </div>
